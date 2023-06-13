463764ee1807b94fe65f01c27e00abb0a936a296
Bobo0808 <wang32625171@gmail.com>
2023-06-13 23:50:02 +0800

    add file

---
+++ Reminder/Reminder/Controllers/RegisterController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Reminder.Models;
+using System.Data.SqlClient;
+
+namespace Reminder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        SqlConnection connection = null;
+
+
+        public RegisterController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            connection = new  SqlConnection(_configuration.GetConnectionString("DefaultConnection").ToString());
+        }
+
+        [HttpPost]
+        [Route("RegisterNewUser")]
+        public Response RegisterNewUser(Users users)
+        {
+            Response response = new Response();
+            DAL dAL = new DAL();
+            response = dAL.Registration(users ,connection);
+            return response;
+        }
+        [HttpPost]
+        [Route("Login")]
+        public Response Login(Users users)
+        {
+            Response response = new Response();
+            DAL dAL = new DAL();
+            response = dAL.Login(users, connection);
+            return response;
+        }
+    }
+}
---
+++ Reminder/Reminder/Models/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace Reminder.Models
+{
+    public class Response
+    {
+        public int StatusCode { get; set; }
+        public string StatusMessage { get; set; }
+
+    }
+}
